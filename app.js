823440aa7d1ad98e7cd19fb7d9187e7dfce985ab
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-03-14 20:08:37 +0530

    Prerequisites
    Added the dice images, index and css file to the repository

---
+++ app.js
@@ -0,0 +1,9 @@
+/* 
+This game consists of 2 players and starts with Player 1.
+During the player's turn, the dice can be rolled as many times as desired until a 1 is rolled or the score is held.
+If you roll a 1, you lose the round score and it is the next player's turn. 
+Otherwise, the scoe you held will be added to the total score.
+The first player to reach a total score of 100 points wins.
+*/
+
+
