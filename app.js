d3afea4ca023c000370a2fe9f15179ee6caab822
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-03-15 00:07:40 +0530

    Hold button
    The functionality of  the roll button was implemented

--- app.js
+++ app.js
@@ -33,14 +33,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             document.getElementById('current-' + activePlayer).textContent = roundScore;
         } else {
             // Next player
-            activePlayer === 0? activePlayer = 1: activePlayer = 0;
-            roundScore = 0;
-            document.getElementById('current-0').textContent = 0;
-            document.getElementById('current-1').textContent = 0;
-            document.querySelector('.player-0-panel').classList.toggle('active');
-            document.querySelector('.player-1-panel').classList.toggle('active');
-            document.querySelector('.dice').style.display = 'none';
+            nextPlayer();
         }
 })
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // 1.Updating the total score
+    scores[activePlayer] += roundScore;
+
+    // 2.Updating the UI
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    // 3. What happens if a player wins the game
+    if (scores[activePlayer] >= 20) {
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Next player
+        nextPlayer();
+    }
+})
+
+function nextPlayer() {
+    activePlayer === 0? activePlayer = 1: activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
