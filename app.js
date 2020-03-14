9a51320117b5725a77ca8701c071d32d69cf24c9
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-03-14 21:51:30 +0530

    Roll button
    Implementing the functionality of the roll button

--- app.js
+++ app.js
@@ -6,16 +6,41 @@ Otherwise, the scoe you held will be added to the total score.
 The first player to reach a total score of 100 points wins.
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = 0;
 document.getElementById('current-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+        // 1.Generating a random number when the dice is rolled
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        // 2.Display the result 
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        // 3. Updating the round score if the number rolled is NOT 1
+        if (dice !== 1) {
+            roundScore += dice;
+            document.getElementById('current-' + activePlayer).textContent = roundScore;
+        } else {
+            // Next player
+            activePlayer === 0? activePlayer = 1: activePlayer = 0;
+            roundScore = 0;
+            document.getElementById('current-0').textContent = 0;
+            document.getElementById('current-1').textContent = 0;
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+            document.querySelector('.dice').style.display = 'none';
+        }
+})
+
 
