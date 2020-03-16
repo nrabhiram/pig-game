67cd7e1a2ebcb025377305cd4dfbac041a60396b
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-03-16 23:49:26 +0530

    Final touches
    The DRY principle and the state variable were successfully implemented

--- app.js
+++ app.js
@@ -8,20 +8,11 @@ The first player to reach a total score of 100 points wins.
 
 var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-gamePlaying = true;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+init(); 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-       if (gamePlaying = true) {
-            // 1.Generating a random number when the dice is rolled
+    if (gamePlaying === true) {
+        // 1.Generating a random number when the dice is rolled
         var dice = Math.floor(Math.random() * 6) + 1;
 
         // 2.Display the result 
@@ -34,34 +25,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             roundScore += dice;
             document.getElementById('current-' + activePlayer).textContent = roundScore;
         } else {
-            // Next player
+           // Next player
             nextPlayer();
         }
-       }
-})
+    }
+});
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    if (gamePlaying = true) {
+    if (gamePlaying === true) {
         // 1.Updating the total score
         scores[activePlayer] += roundScore;
 
         // 2.Updating the UI
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
         document.getElementById('current-' + activePlayer).textContent = 0;
-
+        
         // 3. What happens if a player wins the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.getElementById('name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
-            document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
             // Next player
             nextPlayer();
         }
     }
-})
+});
 
 function nextPlayer() {
     activePlayer === 0? activePlayer = 1: activePlayer = 0;
@@ -73,7 +64,9 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 }
 
-document.querySelector('.btn-new').addEventListener('click', function() {
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
@@ -90,7 +83,7 @@ document.querySelector('.btn-new').addEventListener('click', function() {
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
-})
+}
 
 
 
